1f9d5c3d88339cfbcd678a0ac4502e3e91ed5f8d
aqil2514 <muhamadaqil383@gmail.com>
2023-11-14 12:52:44 +0700

    update origin

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://www.googleapis.com"],
+    origin: ["http://localhost:5173", "https://www.googleapis.com", "https://gamelingo-db.netlify.app"],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
     preflightContinue: true,
