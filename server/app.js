9be62b46bea9b896ba7eeb468fdb65e730b87e3c
aqil2514 <muhamadaqil383@gmail.com>
2023-11-14 13:04:01 +0700

    origin

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 // );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "https://gamelingo-db.netlify.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
