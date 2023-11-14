dfaa7936a7d5d3fca1c0f40bb311fbb2fe0374b1
aqil2514 <muhamadaqil383@gmail.com>
2023-11-14 13:51:10 +0700

    cookies

--- server/app.js
+++ server/app.js
@@ -81,7 +81,12 @@ app.post("/users", async (req, res) => {
     const secretKey = process.env.SECRET_KEY;
     const token = await jwt.sign(payload, secretKey, { expiresIn: "1h" });
 
-    res.cookie("token", token, { maxAge: 60 * 60 * 1000, domain: ".cyclic.app", secure: true });
+    res.cookie("token", token, {
+      maxAge: 60 * 60 * 1000,
+      domain: ".cyclic.app",
+      secure: true,
+      sameSite: "None",
+    });
     return res.json({ token: req.cookies.token, status: 200, message: `Success Login!` });
   } catch (error) {
     console.error(error);
