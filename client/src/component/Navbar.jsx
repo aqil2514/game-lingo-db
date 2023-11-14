dcad3f1636d55b8fa1e3e350c92885b38e0cc7db
aqil2514 <muhamadaqil383@gmail.com>
2023-11-14 13:57:05 +0700

    navbar

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 
   async function token() {
     try {
-      const response = await fetch("http://localhost:3000/token", {
+      const response = await fetch("https://game-lingodb.cyclic.app/token", {
         credentials: "include",
       });
 
